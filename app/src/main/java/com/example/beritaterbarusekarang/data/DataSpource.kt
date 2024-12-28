341e45848b3c4aa4af103803fe5a07a664955154
fransiska <fransiska.febriyanti@students.utdi.ac.id>
2024-12-28 19:39:28 +0700

    Project_UAS_Mobile

--- app/src/main/java/com/example/beritaterbarusekarang/data/DataSpource.kt
+++ app/src/main/java/com/example/beritaterbarusekarang/data/DataSpource.kt
@@ -3,12 +3,23 @@ package com.example.beritaterbarusekarang.data
 import com.example.beritaterbarusekarang.R
 import com.example.beritaterbarusekarang.model.Tampil
 
-class DataSource() {
-    fun loadAffirmations(): List<Tampil> {
-        return listOf<Tampil>(
-            Tampil(R.string.coba1, R.drawable.gambar1),
-            Tampil(R.string.coba2, R.drawable.gambar2),
-            Tampil(R.string.coba3, R.drawable.gambar3),
+class DataSource {
+    // Mengembalikan daftar berita
+    fun loadBerita(): List<Tampil> {
+        return listOf(
+            Tampil(R.string.berita_1, R.drawable.gambar1),
+            Tampil(R.string.berita_2, R.drawable.gambar2),
+            Tampil(R.string.berita_3, R.drawable.gambar3)
+            // Tambahkan berita lainnya sesuai kebutuhan
+        )
+    }
+
+    // Mengembalikan daftar arsip berita
+    fun loadArsipBerita(): List<Tampil> {
+        return listOf(
+            Tampil(R.string.arsip_1, R.drawable.gambar1), // Ganti dengan gambar yang sesuai
+            Tampil(R.string.arsip_2, R.drawable.gambar2), // Ganti dengan gambar yang sesuai
+            Tampil(R.string.arsip_3, R.drawable.gambar3), // Ganti dengan gambar yang sesuai
         )
     }
 }
